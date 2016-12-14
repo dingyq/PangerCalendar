9e3f7d20c2da3e3895ba6e8bc251c1ec3b2ba3b3
panger <pangerding@qq.com>
2016-12-14 08:26:41 +0800

    [add]] 新增农历算法

--- PangerCalendar/ViewController.swift
+++ PangerCalendar/ViewController.swift
@@ -14,24 +14,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
 //        let result = PRDatabaseOperate.shareMgr().queryDataWithDate(year: "1991", month: "5", day: "25")
 //        print(result["fit"]!, result["avoid"]!)
-//        
+//        ---
 //        let result1 = PRDatabaseManager.manager.queryDataWithDate(year: "1991", month: "5", day: "26")
 //        for dic in result1 {
 //            print(dic["fit"]!, dic["avoid"]!)
 //        }
 //        print(PRChineseFestivalsMgr.shareMgr().festival(month: 5, day: 1))
         
+//        ---
         self.view.addSubview(PRCalendarView(frame: CGRect(x: 0, y: 0, width: 100, height: 100)))
-        
+//        ---
 //        let solarTerms = PRSolarTermsFormulaCompute.shareMgr().calculateSoloarTerms(year: 2016)
 //        for solar in solarTerms {
 //            print(solar[kSolarTermMonth]!, solar[kSolarTermDay]!)
 //        }
+    
+//        ---
+//        let index: Int = PRSolarTermsMgr.shareMgr().solartermIndex(year: 2050, month: 7, day: 22)
+//        print(PRSolarTermsMgr.shareMgr().solartermName(index: index))
         
-        let index: Int = PRSolarTermsMgr.shareMgr().solartermIndex(year: 2050, month: 7, day: 22)
-        print(PRSolarTermsMgr.shareMgr().solartermName(index: index))
+//        ---
+        let lunarMgr = PRLunarDateMgr.shareMgr()
+        let lunar: PRLunarDate = lunarMgr.lunardateFromSolar(year: 2016, month: 12, day: 14)
+        print(lunarMgr.lunarDateTianGan(lunarYear: lunar.year))
+        print(lunarMgr.lunarDateDiZhi(lunarYear: lunar.year))
+        print(lunarMgr.lunarDateZodiac(lunarYear: lunar.year))
+        print(lunarMgr.lunarDateMonth(lunarMonth: lunar.month))
+        print(lunarMgr.lunarDateDay(lunarDay: lunar.day))
     }
 
     override func didReceiveMemoryWarning() {
